52cc9233d89d0c090ede7bbe1940033aa8269288
leo <srecherche993@gmail.com>
2020-06-23 20:14:58 +0200

    Initial commit

---
+++ leonelleonel.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+#define N 30
+int main() {
+    float a[10][10], l[10][10], u[10][10], c[10][10];
+    int n = 0, i = 0, j = 0;
+
+    cout << "Enter size of square matrix : " << endl;
+    cin >> n;
+
+    return 0;
+}
